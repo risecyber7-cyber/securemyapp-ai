2bd782f864d1c8c4ca85273bf873c6c94d6d99ca
devh <risecyber7@gmail.com>
2026-04-06 03:24:55 +0530

    Sync apps/api/app/routers/__init__.py

---
+++ apps/api/app/routers/__init__.py
@@ -0,0 +1,8 @@
+from .auth import router as auth_router
+from .fixes import router as fixes_router
+from .issues import router as issues_router
+from .projects import router as projects_router
+from .reports import router as reports_router
+from .scans import router as scans_router
+from .settings import router as settings_router
+from .users import router as users_router
